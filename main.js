af201dab6239ca6e136ce72c41f524105d5e92f8
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-12-06 17:22:04 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,12 @@
 const doc = document.querySelector("body");
 const btn1 = document.querySelector(".btn-primary");
 const btn2 = document.querySelector(".btn-danger");
+const h1 = document.querySelector(".col3 h1");
 
 btn1.addEventListener("click", function () {
   doc.style.backgroundColor = "black";
   doc.style.color = "white";
+  h1.style.color = "white";
 
   btn2.addEventListener("click", function () {
     doc.style.backgroundColor = "#3333";
